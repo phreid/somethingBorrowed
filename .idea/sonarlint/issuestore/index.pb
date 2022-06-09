c0e5adc3dcc80202f186b799d455b440d9de78b8
Anusha <asal15@students.cs.ubc.ca>
2022-06-08 17:43:58 -0700

    added images + css

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,7 @@
 
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+I
+src/components/NavBar.jsx,4/d/4d65cb9d72a970cca77aea256c4d0116a4d0d30c
+D
+src/redux/reducer.js,5/4/54dd5e8bfc8fd8f432e9a2b1a188285e08f22b00
--- src/redux/hardcodedItemsState.js
+++ src/redux/hardcodedItemsState.js
@@ -5,7 +5,7 @@ export let hardcodedItemsState = [
         name: "Bicycle Pump",
         type: "Automotive",
         location: "UBC Campus",
-        image:"",
+        image:"https://media-www.canadiantire.ca/product/playing/cycling/bicycle-accessories/0734828/ccm-bike-foot-pump-4cc883fe-9a59-41dc-a8ce-b3edb89dfe58.png?imwidth=1024",
         description: "Good condition Canadian Tyre gauge bicycle pump with both schrader and presta valve adapters."
     },
     {
@@ -14,7 +14,7 @@ export let hardcodedItemsState = [
         name: "Step Ladder",
         type: "Home",
         location: "UBC Campus",
-        image:"",
+        image:"https://media.istockphoto.com/photos/rendering-ladder-on-white-picture-id699878160?k=20&m=699878160&s=612x612&w=0&h=bVzr6TSmweUDy51lntVhGJfK46l8yV-nvx063RAMt2g=",
         description: "Lightweight foldable step-ladder with safety clasp."
     },
     {
@@ -23,7 +23,7 @@ export let hardcodedItemsState = [
         name: "Hand Blender",
         type: "Kitchen",
         location: "UBC Campus",
-        image:"",
+        image:"https://s7d2.scene7.com/is/image/BedBathandBeyond/28804141436967p?$690$&wid=690&hei=690",
         description: "KitchenAid hand blender with 6 speed settings and whisk attachments."
     }
 ]
