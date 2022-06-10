46f5309b95c2b13857279552568f47d179b45841
Anusha <asal15@students.cs.ubc.ca>
2022-06-09 22:58:09 -0700

    item cards implemented in item container

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,2 +1,28 @@
-export default function ItemCard(props) {
+import React from 'react';
+import {Card, Row} from "react-bootstrap";
+
+function ItemCard(props){
+    return(
+        <Card className="item-card" style={{ width: '' }}>
+            <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
+                <div className="col-md-4">
+                    <Card.Img className="item-image" src={props.image} />
+                </div>
+                <div className="col-md-8">
+                    <Card.Title className="item-name">{props.name}</Card.Title>
+                    <Card.Text className="item-description">
+                        {props.description}
+                    </Card.Text >
+                    <Card.Text className="item-type">
+                        {props.type}
+                    </Card.Text>
+                    <Card.Text className="item-location">
+                        {props.location}
+                    </Card.Text>
+                </div>
+            </Row>
+        </Card>
+    )
 }
+
+export default ItemCard;
