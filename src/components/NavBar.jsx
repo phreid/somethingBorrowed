14a7519eb703d7bbd9bd1640622221db367e363e
phreid <phreid@ualberta.ca>
2022-06-10 21:30:12 -0700

    fix linter dependencies and lint

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
-import '../styles.css';
+import '../styles.css'
 
-export default function NavBar(prop) {
-    return (
-   <>
+export default function NavBar (prop) {
+  return (
+    <>
       <Navbar className="navbar-parent">
-          <Container fluid>
-            <Navbar.Brand href="#home">
-                  {/* <img
-                    src="image"
-                    width="30"
-                    height="30"
-                    className="App-logo"
-                    alt="Something Borrowed"
-                  /> */}
-                  Something Borrowed
-            </Navbar.Brand>
-            <Nav>
-              <Nav.Link to="home">Home</Nav.Link>
-              <Nav.Link to="addItem">Add Item To Profile</Nav.Link>
-            </Nav>
-         </Container>
+        <Container fluid>
+          <Navbar.Brand href="#home">
+            {/* <img
+              src="image"
+              width="30"
+              height="30"
+              className="App-logo"
+              alt="Something Borrowed"
+            /> */}
+            Something Borrowed
+          </Navbar.Brand>
+          <Nav>
+            <Nav.Link to="home">Home</Nav.Link>
+            <Nav.Link to="addItem">Add Item To Profile</Nav.Link>
+          </Nav>
+        </Container>
       </Navbar>
     </>
-    )
+  )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
-import { createStore } from "redux";
-import {reducer} from "./reducer";
+import { createStore } from 'redux'
+import { reducer } from './reducer'
 
-export let store = createStore(reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
+export const store = createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
