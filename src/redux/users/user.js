512d562d8d75c44d5acdbda3aa304d825c09f05d
phreid <phreid@ualberta.ca>
2022-06-15 18:41:07 -0700

    add fake login

--- src/redux/users/user.js
+++ src/redux/users/user.js
@@ -1,13 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const INITIAL_STATE = {
-<<<<<<< HEAD
   username: localStorage.getItem('user'),
   isLoggedIn: 'user' in localStorage
-=======
-  username: '',
-  isLoggedIn: false
->>>>>>> b82c9b2897c171018e34d62e647f4959c7c5c251
 }
 
 const userSlice = createSlice({
