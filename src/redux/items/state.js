b82c9b2897c171018e34d62e647f4959c7c5c251
phreid <phreid@ualberta.ca>
2022-06-15 17:59:37 -0700

    add fake login

--- src/redux/items/state.js
+++ src/redux/items/state.js
@@ -1,7 +1,7 @@
-import cricut from '../images/cricut.jpg'
-import bicyclePump from '../images/bicyclePump.jpg'
-import gardenTools from '../images/gardenTools.jpg'
-import handBlender from '../images/handBlender.jpg'
+import cricut from '../../images/cricut.jpg'
+import bicyclePump from '../../images/bicyclePump.jpg'
+import gardenTools from '../../images/gardenTools.jpg'
+import handBlender from '../../images/handBlender.jpg'
 
 export const baseItems = [
   {
