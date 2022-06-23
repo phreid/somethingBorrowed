62c231e3821912cb4ec0b1104c6591ab73b9fe79
phreid <phreid@ualberta.ca>
2022-06-22 20:48:55 -0700

    add a simple responsive navbar

---
+++ src/components/MarketplacePage.jsx
@@ -0,0 +1,12 @@
+import NavBar from './NavBar'
+
+function MarketplacePage () {
+  return (
+    <>
+      <NavBar />
+      <h1>Marketplace</h1>
+    </>
+  )
+}
+
+export default MarketplacePage
--- src/components/MyItemsPage.jsx
+++ src/components/MyItemsPage.jsx
@@ -6,16 +6,16 @@ import NavBar from './NavBar'
 
 import '../styles.css'
 
-function AddItemPage () {
+function MyItemsPage () {
   return (
     <>
       <NavBar />
-      <h1 className="page-title">Add Item to Profile</h1>
       <div className="grid-container">
         <div className="grid-child page-container">
           <AddItemForm />
         </div>
         <div className="grid-child page-container" id="container-border">
+          <h2>My Items</h2>
           <ItemContainer />
         </div>
       </div>
@@ -23,4 +23,4 @@ function AddItemPage () {
   )
 }
 
-export default AddItemPage
+export default MyItemsPage
