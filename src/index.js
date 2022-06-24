99a3c46605d806d3620bdaabbd485c013640eb3e
imogenem <mcdonaldimo1@gmail.com>
2022-06-23 18:03:41 -0700

    add back loginpage to homepage

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css'
 
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
-import Marketplace from './components/Marketplace'
+import LoginPage from './components/LoginPage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Marketplace />
+      <LoginPage />
     </Provider>
   </React.StrictMode>
 )
