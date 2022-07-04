c900185b7ed88a50c1cd88f991a27f128fd76e30
phreid <phreid@ualberta.ca>
2022-07-03 21:12:58 -0700

    user signup and logout

---
+++ src/components/LogoutPage.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+import { logoutAsync } from '../redux/users/thunks'
+
+export default function LogoutPage () {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  // note: React.StrictMode renders components twice, so
+  // this effect will happen twice, and the second time the call to
+  // the logout API will fail
+  useEffect(() => {
+    const logout = async () => {
+      await dispatch(logoutAsync()).unwrap()
+      navigate('/')
+    }
+    logout()
+  })
+
+  return <></>
+}
