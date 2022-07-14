6cf5078e50e16f19a2f3bfe550ea8f22e3517a46
Anusha <asal15@students.cs.ubc.ca>
2022-07-13 17:39:45 -0700

    created modal for profile page

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,43 +1,50 @@
 import NavBar from './NavBar'
-import { Card, ListGroup, ListGroupItem, Row } from 'react-bootstrap'
+import { Button, Card, ListGroup, ListGroupItem, Modal } from 'react-bootstrap'
 import profile from '../images/profile.png'
 import { useDispatch, useSelector } from 'react-redux'
-import { React, useEffect } from 'react'
+import { React, useEffect, useState } from 'react'
 import { getCurrentUserAsync } from '../redux/users/thunks'
 
 function ProfilePage () {
   const userId = useSelector(state => state.user.currentUserId)
   const dispatch = useDispatch()
+
   useEffect(() => {
     dispatch(getCurrentUserAsync(userId))
   }, [dispatch])
 
   const currentUser = useSelector(state => state.user.currentUser)
-  console.log('=>', currentUser)
+
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
 
   return (
     <>
       <NavBar />
-      <Card.Header className="text-center" as='h4'>My Profile</Card.Header>
       <>
-        <Card className="text-center" style={{ width: '' }}>
-          <Row className="card-example d-flex flex-row flex-nowrap overflow-auto">
-            <div className="col-md-4">
-              <Card.Img variant="top" src={profile} />
-              <Card.Body>
-                <Card.Title>{currentUser.username}</Card.Title>
-                <Card.Text>{currentUser.bio}</Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroupItem>{currentUser.email}</ListGroupItem>
-                <ListGroupItem>{currentUser.location}</ListGroupItem>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="#">Edit Profile</Card.Link>
-                <Card.Link href="#">Sign Out</Card.Link>
-              </Card.Body>
-            </div>
-          </Row>
+        <Card className="profile-card" style={{ width: '' }}>
+          <Card.Body className="text-center">
+            <Card.Img className="profile-img" variant="center" width="80" src={profile} />
+            <Card.Title>{currentUser.username}</Card.Title>
+            <Card.Text>{currentUser.bio}</Card.Text>
+            <ListGroup className="list-group-flush center">
+              <ListGroupItem>{currentUser.email}</ListGroupItem>
+              <ListGroupItem>{currentUser.location}</ListGroupItem>
+            </ListGroup>
+            <Button variant="outline-secondary" onClick={handleShow}>Edit Profile</Button>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+              <Modal.Footer>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </Card.Body>
         </Card>
       </>
     </>
--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 )
