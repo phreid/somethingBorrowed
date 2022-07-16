73dc97fcfee07780928f388270f974dc57ca9323
imogenem <mcdonaldimo1@gmail.com>
2022-07-15 20:03:52 -0700

    remove backend temporarily

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import itemsReducer from './items/items'
 import userReducer from './users/user'
-import marketsplaceItemsReducer from './items/marketsplaceItems'
 
 export const store = configureStore({
   reducer: {
     items: itemsReducer,
-    user: userReducer,
-    m: marketsplaceItemsReducer
+    user: userReducer
   },
   devTools: true
 })
