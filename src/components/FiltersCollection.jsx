a857c751f8aec2625c215e0e2799afea7fcf5954
cailors <106421786+cailors@users.noreply.github.com>
2022-07-24 18:16:51 -0700

    Add files via upload

--- src/components/FiltersCollection.jsx
+++ src/components/FiltersCollection.jsx
@@ -1,64 +1,92 @@
 import '../styles/search.css'
 import React, { useState } from 'react'
-import { applySearchNameAsync, getAllItemsAsync } from '../redux/items/thunks'
+import { applySearchNameAsync, getAllItemsAsync, applyFiltersAsync } from '../redux/items/thunks'
 import { useDispatch } from 'react-redux'
-import { Button } from 'react-bootstrap'
+import { Button, ButtonToolbar, InputGroup, Form } from 'react-bootstrap'
+import { ITEM_TYPES, RATINGS } from '../constants'
 
 function FiltersCollection () {
   const dispatch = useDispatch()
   const [searchInput, setSearchInput] = useState('')
+  const [itemType, setItemType] = useState('Select item type...')
+  const [itemRate, setItemRate] = useState('Unrated')
+
+  function handleCategory (event) {
+    setItemType(event.target.value)
+  }
+
+  function handleRating (event) {
+    setItemRate(event.target.value)
+  }
+
+  function handleApplyFilter () {
+    const jsonObj = JSON.stringify({ rating: itemRate, type: itemType })
+    console.log(jsonObj + '  line32')
+    dispatch(applyFiltersAsync(jsonObj))
+  }
+
+  const handleClearFilters = () => {
+    setItemType('Select item type...')
+    setItemRate('Unrated')
+    dispatch(getAllItemsAsync())
+  }
+
+  const itemTypeDropdowns = Object.values(ITEM_TYPES).map((type) => {
+    return <option key={type}>{type}</option>
+  })
+
+  const ratingDropdowns = Object.values(RATINGS).map((rate) => {
+    return <option key={rate}>{rate}</option>
+  })
 
   const handleClearSearch = () => {
-    setSearchInput('')
+    dispatch(getAllItemsAsync())
   }
 
   const handleApplySearch = () => {
-	if (searchInput === '') {
-		dispatch(getAllItemsAsync())
-	} else {
-		dispatch(applySearchNameAsync(searchInput))
-	}
-	setSearchInput('')
+    if (searchInput === '') {
+      dispatch(getAllItemsAsync())
+    } else {
+      dispatch(applySearchNameAsync(searchInput))
+    }
+  }
+
+  const handleClearInput = () => {
+    setSearchInput('')
   }
 
   return (
     <>
-      <div className="search">
-        <div className="searchBar">
-		  <div className="mb-2">
-			<input
-				id = "searchInput"
-				type="text"
-				placeholder='Press Enter To Search'
-				value = {searchInput}
-				onChange={(e) => setSearchInput(e.target.value)}
-			/>
-			{' '}
-			<Button size="sm" variant="outline-primary" type="submit" className="me-1 button apply-search" onClick={handleApplySearch}>Apply Search</Button>
-		  	{' '}
-		  	<Button  size="sm" variant="outline-primary" type="submit" className="me-1 button clear-search" onClick={handleClearSearch}>Clear Search</Button>
-		  </div>
-		  
-        </div>
-        <div className="filters">
-          <div className='filter'>
-            <b className='filterText'>Category</b>
-            <div className='categories'>
-              <a ><label htmlFor="Kitchen" className='label'>Kitchen</label><input type="checkbox" id="Kitchen" className='checkbox'/></a>
-              <a ><label htmlFor="Outdoor" className='label'>Outdoor</label><input type="checkbox" id="Outdoor" className='checkbox'/></a>
-              <a ><label htmlFor="Tools" className='label'>Tools</label><input type="checkbox" id="Tools" className='checkbox'/></a>
-              <a ><label htmlFor="DIY" className='label'>DIY</label><input type="checkbox" id="DIY" className='checkbox'/></a>
-            </div>
-          </div>
-          <div className='filter'>
-            <b className='filterText'>Location</b>
-            <div className='location'>
-              <a ><label htmlFor="UBC"> UBC </label><input type="checkbox" id="UBC" disabled={true}/></a>
-            </div>
-          </div>
-        </div>
-
-      </div>
+      <ButtonToolbar className="d-flex justify-content-center">
+        <InputGroup className="me-3">
+          <Form.Control
+            type="text"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            placeholder="Enter your search"
+          />
+          <Button variant="outline-secondary" onClick={handleClearInput}>Clear</Button>
+        </InputGroup>
+        <Button className="me-1" variant="outline-primary" onClick={handleApplySearch}>Search</Button>
+        <Button variant="outline-secondary" onClick={handleClearSearch}>Reset Search</Button>
+      </ButtonToolbar>
+      <br></br>
+      <Form.Group >
+        <Form.Select value={itemType} onChange={handleCategory}>
+          <option>Select item type...</option>
+          {itemTypeDropdowns}
+        </Form.Select>
+      </Form.Group>
+      <Form.Group >
+        <Form.Select value={itemRate} onChange={handleRating}>
+          {ratingDropdowns}
+        </Form.Select>
+      </Form.Group>
+      <br></br>
+      <a className="search_input">
+        <Button variant="outline-primary" type="submit" className="me-1 button apply-filter" onClick={handleApplyFilter}>Apply Filters</Button>
+        <Button variant="outline-secondary" className="me-1 button clear-filter" onClick={handleClearFilters}>Clear Filters</Button>
+      </a>
     </>
   )
 }
