1752ee43924bc5c87d746bafd8cf86b9d93e33bb
imogenem <mcdonaldimo1@gmail.com>
2022-07-29 20:03:05 -0700

    add calendar

--- frontend/src/components/requests/RequestModal.jsx
+++ frontend/src/components/requests/RequestModal.jsx
@@ -5,10 +5,13 @@ import Form from 'react-bootstrap/Form'
 import Modal from 'react-bootstrap/Modal'
 import Row from 'react-bootstrap/Row'
 import CounterInput from 'react-bootstrap-counter'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
 
 import '../../styles.css'
 
 export default function RequestModal (props) {
+  const [dateNeeded, onDateNeeded] = useState(new Date())
   const [requestMessage, setRequestMessage] = useState('')
 
   const handleClose = () => {
@@ -24,6 +27,7 @@ export default function RequestModal (props) {
   return (
     <>
       <Modal
+        size="lg"
         show={props.requestOpen}
         onHide={() => props.setShow(false)}
         backdrop="static"
@@ -37,16 +41,25 @@ export default function RequestModal (props) {
             <Row>
               <Form.Group as={Col} sm >
                 <Form.Label>Item Name: <strong>{props.name}</strong></Form.Label>
-                <Form.Label>How many days would you like to borrow it for?</Form.Label>
-                <CounterInput value={1} min={1} max={100} />
-                <br></br>
               </Form.Group>
             </Row>
-            <Form.Group >
-              <Form.Label>Additional notes</Form.Label>
-              <Form.Control as="textarea" rows={3} placeholder="Tell the owner why you would like to borrow this item" value={requestMessage}
-                onChange={(e) => setRequestMessage(e.target.value)}></Form.Control>
-            </Form.Group>
+            <Row>
+              <Form.Group as={Col} sm >
+                <Form.Label>How many days would you like to borrow it for?</Form.Label>
+                <CounterInput value={1} min={1} max={100} />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group as={Col} sm >
+                <Form.Label>What day do you need the item?</Form.Label>
+                <Calendar onChange={onDateNeeded} value={dateNeeded} />
+              </Form.Group>
+              <Form.Group >
+                <Form.Label>Additional notes</Form.Label>
+                <Form.Control as="textarea" rows={3} placeholder="Tell the owner why you would like to borrow this item" value={requestMessage}
+                  onChange={(e) => setRequestMessage(e.target.value)}></Form.Control>
+              </Form.Group>
+            </Row>
             <Button variant="primary" type="submit" className="me-1"
               onClick={handleSubmit}>Submit Request</Button>
             <Button variant="danger" type="reset">Reset</Button>
