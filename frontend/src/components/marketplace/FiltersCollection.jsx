c2e31a2fdea6d6bf5c7d12b5d9ca87abc92e898c
cailors <cailors@students.cs.ubc.ca>
2022-07-31 15:00:58 -0700

    frontend of filtering by status

--- frontend/src/components/marketplace/FiltersCollection.jsx
+++ frontend/src/components/marketplace/FiltersCollection.jsx
@@ -2,19 +2,29 @@ import React, { useState } from 'react'
 import { Button, ButtonToolbar, InputGroup, Form } from 'react-bootstrap'
 import { useDispatch } from 'react-redux'
 
-import { ITEM_TYPES, RATINGS } from '../../constants'
+import { ITEM_TYPES, RATINGS, STATUS } from '../../constants'
 import { getItemsAsync } from '../../redux/items/thunks'
 
 import '../../styles/search.css'
 
-const DEFAULT_TYPE_OPTION = 'All item types!'
-const DEFAULT_RATING_OPTION = 'All ratings!'
+const DEFAULT_TYPE_OPTION = 'All item types'
+const DEFAULT_RATING_OPTION = 'All ratings'
+const DEFAULT_STATUS_OPTION = 'All status'
 
 function FiltersCollection () {
   const dispatch = useDispatch()
   const [searchInput, setSearchInput] = useState('')
   const [itemType, setItemType] = useState('')
   const [itemRating, setItemRating] = useState('')
+  const [itemSatus, setItemStatus] = useState('')
+
+  function handleStatus (event) {
+    if (event.target.value === DEFAULT_STATUS_OPTION) {
+      setItemStatus('')
+    } else {
+      setItemStatus(event.target.value)
+    }
+  }
 
   function handleCategory (event) {
     if (event.target.value === DEFAULT_TYPE_OPTION) {
@@ -35,7 +45,8 @@ function FiltersCollection () {
   function handleApplyFilter () {
     const rating = itemRating || undefined
     const type = itemType || undefined
-    dispatch(getItemsAsync({ rating, type }))
+    const status = itemSatus || undefined
+    dispatch(getItemsAsync({ rating, type, status }))
   }
 
   const handleClearFilters = () => {
@@ -50,6 +61,10 @@ function FiltersCollection () {
 
   const ratingDropdowns = Object.values(RATINGS).map((rate) => {
     return <option key={rate}>{rate}</option>
+  })
+
+  const statusDropdowns = Object.values(STATUS).map((status) => {
+    return <option key={status}>{status}</option>
   })
 
   const handleClearSearch = () => {
@@ -91,6 +106,12 @@ function FiltersCollection () {
         <Form.Select value={itemRating} onChange={handleRating}>
           <option>{DEFAULT_RATING_OPTION}</option>
           {ratingDropdowns}
+        </Form.Select>
+      </Form.Group>
+      <Form.Group >
+        <Form.Select value={itemSatus} onChange={handleStatus}>
+          <option>{DEFAULT_STATUS_OPTION}</option>
+          {statusDropdowns}
         </Form.Select>
       </Form.Group>
       <br></br>
