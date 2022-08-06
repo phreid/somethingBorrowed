a44619d541fff25682085d8517099b193f097ab2
Anusha <asal15@students.cs.ubc.ca>
2022-08-06 13:57:53 -0700

    removed location from edit user modal

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   ITEM_TYPES: require('./item-types'),
-  LOCATIONS: require('./locations'),
   STATUS: require('./status'),
   RATINGS: require('./ratings'),
   REQUEST_STATUS: require('./request-status')
