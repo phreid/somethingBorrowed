1c40bf4228d46b0609099226e38c5008ad32eda5
nyananu <35955246+nyananu@users.noreply.github.com>
2022-08-06 20:35:51 -0700

    Revert "map to dev"

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -2,7 +2,6 @@ module.exports = {
   ITEM_TYPES: require('./item-types'),
   LOCATIONS: require('./locations'),
   STATUS: require('./status'),
-  LOCATIONS: require('./locations'),
   RATINGS: require('./ratings'),
   REQUEST_STATUS: require('./request-status')
 }
