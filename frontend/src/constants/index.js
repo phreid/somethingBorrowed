1a569f176daed46b927740a726a822c11ecc2557
Anusha <asal15@students.cs.ubc.ca>
2022-08-06 20:30:25 -0700

    map to dev

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   ITEM_TYPES: require('./item-types'),
   STATUS: require('./status'),
+  LOCATIONS: require('./locations'),
   RATINGS: require('./ratings'),
   REQUEST_STATUS: require('./request-status')
 }
