02fe3ea9890c92ebfc49d5be2b8a556489c70eac
nyananu <35955246+nyananu@users.noreply.github.com>
2022-08-06 18:46:23 -0700

    Revert "(done) Updated map"

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,16 +6,13 @@ import App from './App'
 import { store } from './redux/store'
 
 import './index.css'
-import { LoadScript } from '@react-google-maps/api'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <LoadScript googleMapsApiKey = {process.env.REACT_APP_MAPS_API_KEY} >
-          <App />
-        </LoadScript>
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
