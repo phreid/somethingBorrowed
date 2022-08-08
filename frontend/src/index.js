e121d0b9f7916d81d33c1db27534c1e86084d91a
phreid <phreid@ualberta.ca>
2022-08-08 00:47:16 -0700

    linting error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { store } from './redux/store'
 import { LoadScript } from '@react-google-maps/api'
+import {Button} from 'react-bootstrap'
 
 import './index.css'
 
