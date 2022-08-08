a857f226d44621be356c0a091f127284a56d76b3
Anusha <asal15@students.cs.ubc.ca>
2022-08-08 11:56:02 -0700

    my-items page is responsive

--- frontend/src/components/my-items/MyItemsPage.jsx
+++ frontend/src/components/my-items/MyItemsPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Tabs, Tab } from 'react-bootstrap'
+import { Tabs, Tab, Container } from 'react-bootstrap'
 
 import NavBar from '../common/NavBar'
 import AddItemForm from './AddItemForm'
@@ -7,6 +7,8 @@ import ItemContainer from './ItemContainer'
 import UserHistoryPage from './UserHistoryPage'
 
 import '../../styles.css'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function MyItemsPage () {
   return (
@@ -14,15 +16,21 @@ function MyItemsPage () {
       <NavBar />
       <Tabs fill justify className='mt-3 mb-3' defaultActiveKey='my-items'>
         <Tab className='tab-content' eventKey='my-items' title='Manage My Items'>
-          <div className="grid-container">
-            <div className="grid-child item-form-container">
-              <AddItemForm />
-            </div>
-            <div className="grid-child page-container">
-              <h2 className='add-item-heading'>My Items</h2>
-              <ItemContainer />
-            </div>
-          </div>
+          <Container fluid className="container">
+            <Row>
+              <Col xs={12} sm={6} lg={6}>
+                <div className="item-form-container">
+                  <AddItemForm />
+                </div>
+              </Col>
+              <Col xs={12} sm={6} lg={6}>
+                <div className="page-container">
+                  <h2 className='add-item-heading'>My Items</h2>
+                  <ItemContainer />
+                </div>
+              </Col>
+            </Row>
+          </Container>
         </Tab>
         <Tab className='tab-content' eventKey='my-history' title='Borrowing History'>
           <UserHistoryPage />
